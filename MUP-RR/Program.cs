baead327f6e6941f11c41a08c370faae8998ad0c
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-04-04 15:35:13 +0100

    DotNet Main

---
+++ MUP-RR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MUP_RR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
