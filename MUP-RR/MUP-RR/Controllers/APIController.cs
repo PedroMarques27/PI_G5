02ef9a55a804738c9ba1a2ab6c271960f2146061
Rui-FMF <ruifmf2000@gmail.com>
2021-05-08 22:01:39 +0100

    changed api endpoint

--- MUP-RR/MUP-RR/Controllers/APIController.cs
+++ MUP-RR/MUP-RR/Controllers/APIController.cs
@@ -1,8 +1,9 @@
-﻿
+﻿using System;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MUP_RR.Models;
+using System.Collections.Generic;
 
 
 namespace MUP_RR.Controllers
@@ -16,12 +17,14 @@ namespace MUP_RR.Controllers
             return "This is Index action method of StudentController";
         }  
   
-        [HttpGet]
-        [Route("api/v1/notify/{iupi}")]
-        public string GetAuthor(string iupi)
+        [HttpPost]
+        [Route("api/v1/notify")]
+        public IActionResult UpdatePermissions([FromBody] RCUNotification notification)
         {
-            currentProgram.UpdateProfile(iupi);
-            return "This Is "+ iupi;
+            //currentProgram.UpdateProfile(iupi);
+            Console.WriteLine(notification.pairs[0][0]);
+            //Debug.WriteLine(pairs);
+            return Ok();
         }
     }
 
